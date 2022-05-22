cadd0c6ef79a1cefeeff02f633beaf37ae5788ab
maxwaldor <74843287+maxwaldor@users.noreply.github.com>
2022-05-21 23:31:27 -0700

    Solid Textures

--- material.h
+++ material.h
@@ -3,6 +3,8 @@
 
 #include "rtweekend.h"
 
+#include "texture.h"
+
 struct hit_record;
 
 class material {
@@ -15,7 +17,8 @@ class material {
 
 class lambertian : public material {
 	public:
-		lambertian(const color& a) : albedo(a) {}
+		lambertian(const color& a) : albedo(make_shared<solid_color>(a)) {}
+		lambertian(shared_ptr<texture> a) : albedo(a) {}
 
 		virtual bool scatter(
 			const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
@@ -27,12 +30,12 @@ class lambertian : public material {
 				scatter_direction = rec.normal;
 
 			scattered = ray(rec.p, scatter_direction, r_in.time());
-			attenuation = albedo;
+			attenuation = albedo->value(rec.u, rec.v, rec.p);
 			return true;
 		}
 
 	public:
-		color albedo;
+		shared_ptr<texture> albedo;
 };
 
 

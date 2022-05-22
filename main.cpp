1f069bbbacc5a1d9324900e7c875dd74077821cf
maxwaldor <74843287+maxwaldor@users.noreply.github.com>
2022-05-21 18:46:50 -0700

    Motion Blur

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "color.h"
 #include "hittable_list.h"
 #include "material.h"
+#include "moving_sphere.h"
 #include "sphere.h"
 
 #include <iostream>
@@ -47,7 +48,9 @@ hittable_list random_scene() {
                     // diffuse
                     auto albedo = color::random() * color::random();
                     sphere_material = make_shared<lambertian>(albedo);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                    auto center2 = center + vec3(0, random_double(0, .5), 0);
+                    world.add(make_shared<moving_sphere>(
+                        center, center2, 0.0, 1.0, 0.2, sphere_material));
                 }
                 else if (choose_mat < 0.95) {
                     // metal
@@ -81,10 +84,9 @@ int main() {
 
     // Image
 
-    const auto aspect_ratio = 3.0 / 2.0;
-    const int image_width = 1200;
-    const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 500;
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int samples_per_pixel = 100;
     const int max_depth = 50;
 
     // World
@@ -97,8 +99,10 @@ int main() {
     vec3 vup(0, 1, 0);
     auto dist_to_focus = 10.0;
     auto aperture = 0.1;
+    int image_height = static_cast<int>(image_width / aspect_ratio);
 
-    camera cam(lookfrom, lookat, vup, 20.0, aspect_ratio, aperture, dist_to_focus);
+
+    camera cam(lookfrom, lookat, vup, 20.0, aspect_ratio, aperture, dist_to_focus, 0.0, 1.0);
 
     // Render
 
